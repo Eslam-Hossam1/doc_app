c6cad01424ff908f931671158b31580654dbbe06
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-21 20:07:20 +0300

    chore : add app text styles class

---
+++ lib/core/utils/app_text_styles.dart
@@ -0,0 +1,207 @@
+library;
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+abstract class AppTextStyles {
+  static final TextStyle bold48 = TextStyle(
+    fontSize: 48.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bold40 = TextStyle(
+    fontSize: 40.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bold32 = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bold24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w700,
+    letterSpacing: -0.48,
+  );
+
+  static final TextStyle bold20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bold18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bold16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bold14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bold12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle semibold40 = TextStyle(
+    fontSize: 40.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold36 = TextStyle(
+    fontSize: 36.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold28 = TextStyle(
+    fontSize: 28.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semibold11 = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle medium32 = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium26 = TextStyle(
+    fontSize: 26.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium11 = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium10 = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle medium8 = TextStyle(
+    fontSize: 8.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static final TextStyle regular32 = TextStyle(
+    fontSize: 32.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular26 = TextStyle(
+    fontSize: 26.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular24 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular20 = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular18 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular16 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular14 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular12 = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular11 = TextStyle(
+    fontSize: 11.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle regular10 = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeight.w400,
+  );
+}
