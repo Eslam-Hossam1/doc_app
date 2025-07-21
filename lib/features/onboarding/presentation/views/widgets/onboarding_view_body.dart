9debc45c5deafcf76209ab3f94a3b7f16267d380
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-22 02:02:06 +0300

    feat : create onboardingViewBody widget

---
+++ lib/features/onboarding/presentation/views/widgets/onboarding_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:doc_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class OnboardingViewBody extends StatelessWidget {
+  const OnboardingViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: [],
+    );
+  }
+}
