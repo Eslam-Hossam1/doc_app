68d9b21846690929154a8b7ba25a8325bee84eab
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-22 05:46:06 +0300

    feat : implement get start button on pressed to go  to login view

--- lib/features/onboarding/presentation/views/widgets/get_start_button.dart
+++ lib/features/onboarding/presentation/views/widgets/get_start_button.dart
@@ -1,8 +1,10 @@
+import 'package:doc_app/core/routing/routes_paths.dart';
 import 'package:doc_app/core/theming/app_colors.dart';
 import 'package:doc_app/core/utils/app_text_styles.dart';
 import 'package:doc_app/core/widgets/custom_text_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
 
 class GetStartButton extends StatelessWidget {
   const GetStartButton({super.key});
@@ -21,7 +23,7 @@ class GetStartButton extends StatelessWidget {
           textAlign: TextAlign.center,
         ),
         onPressed: () {
-          //todo: going to signup or login view
+          context.go(RoutePaths.login);
         },
       ),
     );
