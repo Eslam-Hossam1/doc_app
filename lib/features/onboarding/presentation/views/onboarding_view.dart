eac28365143778bb3475d348d190f4ad727e4768
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-22 04:13:36 +0300

    feate : add onboarding view body to onboarding view

--- lib/features/onboarding/presentation/views/onboarding_view.dart
+++ lib/features/onboarding/presentation/views/onboarding_view.dart
@@ -1,3 +1,4 @@
+import 'package:doc_app/features/onboarding/presentation/views/widgets/onboarding_view_body.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingView extends StatelessWidget {
@@ -6,6 +7,7 @@ class OnBoardingView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: OnboardingViewBody(),
     );
   }
 }
