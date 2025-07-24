47cc136658150889237689b1c14a72ad2a22cddd
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-24 19:43:28 +0300

    feat : create loginView

---
+++ lib/features/login/presentation/views/login_view.dart
@@ -0,0 +1,94 @@
+import 'package:doc_app/core/theming/app_colors.dart';
+import 'package:doc_app/core/theming/app_text_styles.dart';
+import 'package:doc_app/core/utils/spacing.dart';
+import 'package:doc_app/core/widgets/custom_text_form_field.dart';
+import 'package:doc_app/features/login/presentation/views/widgets/login_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'widgets/already_have_account_text.dart';
+import 'widgets/terms_and_conditions_text.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final formKey = GlobalKey<FormState>();
+  bool isObscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Welcome Back',
+                  style: AppTextStyles.bold24.copyWith(
+                    color: AppColors.primary100,
+                  ),
+                ),
+                verticalSpace(8),
+                Text(
+                  'We\'re excited to have you back, can\'t wait to see what you\'ve been up to since you last logged in.',
+                  style:
+                      AppTextStyles.regular14.copyWith(color: AppColors.grey70),
+                ),
+                verticalSpace(36),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    children: [
+                      const CustomTextFormField(hintText: 'Email'),
+                      verticalSpace(18),
+                      CustomTextFormField(
+                        hintText: 'Password',
+                        isObscureText: isObscureText,
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              isObscureText = !isObscureText;
+                            });
+                          },
+                          child: Icon(
+                            isObscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                        ),
+                      ),
+                      verticalSpace(24),
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: Text(
+                          'Forgot Password?',
+                          style: AppTextStyles.regular12.copyWith(
+                            color: AppColors.primary100,
+                          ),
+                        ),
+                      ),
+                      verticalSpace(40),
+                      LoginButton(),
+                      verticalSpace(16),
+                      const TermsAndConditionsText(),
+                      verticalSpace(60),
+                      const AlreadyHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
