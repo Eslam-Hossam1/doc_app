8240277b15285edcde2c824a285229b320fc1747
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-25 16:59:24 +0300

    chore(api) : setup api layer

---
+++ lib/core/api/api_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:doc_app/core/api/endpoints.dart';
+import 'package:doc_app/features/login/data/models/login_request_body.dart';
+import 'package:doc_app/features/login/data/models/login_response.dart';
+
+import 'package:retrofit/retrofit.dart';
+
+part 'api_service.g.dart';
+
+@RestApi(baseUrl: Endpoints.apiBaseUrl)
+abstract class ApiService {
+  factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(Endpoints.login)
+  Future<LoginResponse> login(
+    @Body() LoginRequestBody loginRequestBody,
+  );
+}
