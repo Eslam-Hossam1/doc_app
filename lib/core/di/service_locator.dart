f088f9789886d6b160f347aa9f225e3ef4b5ab50
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-27 01:19:31 +0300

    feat(di): register sign up repo impl

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,8 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:doc_app/core/api/api_service.dart';
 import 'package:doc_app/core/api/dio_factory.dart';
-import 'package:doc_app/features/login/data/repos/login_repo.dart';
 import 'package:doc_app/features/login/data/repos/login_repo_impl.dart';
+import 'package:doc_app/features/sign_up/data/repos/sign_up_repo_impl.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt getIt = GetIt.instance;
@@ -13,6 +13,11 @@ void setupServiceLocator() {
   //login
   getIt.registerLazySingleton<LoginRepoImpl>(
     () => LoginRepoImpl(
+      apiService: getIt<ApiService>(),
+    ),
+  );
+  getIt.registerLazySingleton<SignupRepoImpl>(
+    () => SignupRepoImpl(
       apiService: getIt<ApiService>(),
     ),
   );
