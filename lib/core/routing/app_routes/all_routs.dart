71ae361245bf1b39773dbdb8be7e759dc593b5e9
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-26 23:20:50 +0300

    feat(home): create simple home view for testing login flow

--- lib/core/routing/app_routes/all_routs.dart
+++ lib/core/routing/app_routes/all_routs.dart
@@ -1,4 +1,5 @@
 import 'package:doc_app/core/routing/app_routes/auth_routs.dart';
+import 'package:doc_app/core/routing/app_routes/home_route.dart';
 import 'package:doc_app/core/routing/app_routes/onboarding_route.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,5 +7,6 @@ class AppRoutes {
   static List<RouteBase> routes = [
     ...OnboardingRoute.routes,
     ...AuthRoutes.routes,
+    ...HomeRoute.routes,
   ];
 }
