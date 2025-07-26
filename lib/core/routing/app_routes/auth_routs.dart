222541bed7d407236f94f9ff755cd8056c8a7348
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-27 00:22:23 +0300

    feat(login): provide login cubit

--- lib/core/routing/app_routes/auth_routs.dart
+++ lib/core/routing/app_routes/auth_routs.dart
@@ -1,11 +1,20 @@
+import 'package:doc_app/core/di/service_locator.dart';
 import 'package:doc_app/core/routing/routes_paths.dart';
+import 'package:doc_app/features/login/data/repos/login_repo_impl.dart';
+import 'package:doc_app/features/login/presentation/manager/cubit/login_cubit.dart';
 import 'package:doc_app/features/login/presentation/views/login_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class AuthRoutes {
   static GoRoute login = GoRoute(
     path: RoutePaths.login,
-    builder: (context, state) => const LoginView(),
+    builder: (context, state) => BlocProvider(
+      create: (context) => LoginCubit(
+        loginRepo: getIt<LoginRepoImpl>(),
+      ),
+      child: const LoginView(),
+    ),
   );
 
   static List<GoRoute> routes = [
