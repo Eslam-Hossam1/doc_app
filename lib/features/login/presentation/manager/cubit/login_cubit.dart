06d47131d2bec20258749d3a658b85f8d4ba8e87
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-26 22:39:06 +0300

    feat(login cubit): create and implement login cubit/states

---
+++ lib/features/login/presentation/manager/cubit/login_cubit.dart
@@ -0,0 +1,32 @@
+import 'package:bloc/bloc.dart';
+import 'package:doc_app/core/api/api_result.dart';
+import 'package:doc_app/features/login/data/models/login_request_body.dart';
+import 'package:doc_app/features/login/data/models/login_response.dart';
+import 'package:doc_app/features/login/data/repos/login_repo.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'login_state.dart';
+part 'login_cubit.freezed.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo _loginRepo;
+  LoginCubit({required LoginRepo loginRepo})
+      : _loginRepo = loginRepo,
+        super(LoginState.initial());
+  Future<void> login({
+    required LoginRequestBody loginRequestBody,
+  }) async {
+    emit(LoginState.loading());
+    ApiResult<LoginResponse> result =
+        await _loginRepo.login(loginRequestBody: loginRequestBody);
+    switch (result) {
+      case ApiSuccess<LoginResponse>():
+        emit(LoginState.success(result));
+        break;
+      case ApiFailure<LoginResponse>():
+        emit(LoginState.failure(
+            error: result.errorHandler.apiErrorModel.message ?? ''));
+        break;
+    }
+  }
+}
