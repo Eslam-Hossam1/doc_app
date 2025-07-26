7d3958707a0fd0f114bf54fec5890bcddacbdcee
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-27 01:24:22 +0300

    feat(routing): add sign up route & provide sign up cubit

--- lib/core/routing/app_routes/auth_routs.dart
+++ lib/core/routing/app_routes/auth_routs.dart
@@ -3,6 +3,9 @@ import 'package:doc_app/core/routing/routes_paths.dart';
 import 'package:doc_app/features/login/data/repos/login_repo_impl.dart';
 import 'package:doc_app/features/login/presentation/manager/cubit/login_cubit.dart';
 import 'package:doc_app/features/login/presentation/views/login_view.dart';
+import 'package:doc_app/features/sign_up/data/repos/sign_up_repo_impl.dart';
+import 'package:doc_app/features/sign_up/logic/sign_up_cubit.dart';
+import 'package:doc_app/features/sign_up/ui/sign_up_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
@@ -16,8 +19,16 @@ class AuthRoutes {
       child: const LoginView(),
     ),
   );
+  static GoRoute signUp = GoRoute(
+    path: RoutePaths.signUp,
+    builder: (context, state) => BlocProvider(
+      create: (context) => SignupCubit(getIt<SignupRepoImpl>()),
+      child: const SignUpView(),
+    ),
+  );
 
   static List<GoRoute> routes = [
     login,
+    signUp,
   ];
 }
--- lib/core/routing/routes_paths.dart
+++ lib/core/routing/routes_paths.dart
@@ -2,4 +2,5 @@ abstract class RoutePaths {
   static const onboarding = "/onboarding";
   static const login = "/login";
   static const home = "/home";
+  static const signUp = "/sign-up";
 }
