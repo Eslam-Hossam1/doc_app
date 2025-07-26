f639c58441e9b5d820a193733dfa30483bfa00e1
Eslam Hossam <eslamhossam.tech@gmail.com>
2025-07-27 01:09:20 +0300

    feat(api): add signup endpoint

--- lib/core/api/endpoints.dart
+++ lib/core/api/endpoints.dart
@@ -2,4 +2,5 @@ abstract class Endpoints {
   static const String apiBaseUrl = "https://vcare.integration25.com/api/";
 
   static const String login = "auth/login";
+  static const String signUp = "auth/register";
 }
